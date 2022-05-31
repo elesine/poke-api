5de6534f50a19f8f71b396b34905b4aebd9430e3
elesine <danitza.canasa@gmail.com>
2022-05-31 17:47:10 -0500

    delete pokeApi-await-async copy

--- pokeApi-await-async.js
+++ pokeApi-await-async.js
@@ -3,6 +3,20 @@ import fetch from "node-fetch";
 const pokemon1 = process.argv[2];
 const pokemon2 = process.argv[3];
 
+function CustomError(from_, status_ ) {
+  let instance = new Error(from_, status_);
+  instance.from = from_;
+  instance.status = status_;
+  instance.message = `${instance.from}, ${
+        instance.status == 404
+      ? "It does not exist. Enter the name correctly!"
+      : instance.status >= 500
+      ? "At this moment the service is not available!"
+      : "HTTP error!"
+  } \nStatus: ${instance.status}`
+  return instance;
+}
+
 const versus = async (pokemon1, pokemon2) => {
   try {
     if (pokemon1 == undefined || pokemon2 == undefined) {
@@ -12,62 +26,29 @@ const versus = async (pokemon1, pokemon2) => {
       `https://pokeapi.co/api/v2/pokemon/${pokemon1}`
     );
     if (!pokemon1Response.ok) {
-      throw new Error(
-        `First pokemon: ${
-          pokemon1Response.status == 404
-            ? "It does not exist, write the name correctly!"
-            : pokemon1Response.status >= 500
-            ? "At this moment the service is not available!"
-            : "HTTP error!"
-        } \nStatus: ${pokemon1Response.status}`
-      );
+      throw new CustomError('First pokemon:', pokemon1Response.status);
     } 
     const pokemon1Json = await pokemon1Response.json();
     const pokemon1PrimaryTypeName = pokemon1Json.types[0].type.name;
     const pokemon1PrimaryTypeUrl = pokemon1Json.types[0].type.url;
     const pokemon1TypeResponse = await fetch(pokemon1PrimaryTypeUrl);
     if (!pokemon1TypeResponse.ok) {
-      throw new Error(
-        `First pokemon Type: ${
-          pokemon1TypeResponse.status == 404
-            ? "It does not exist."
-            : pokemon1TypeResponse.status >= 500
-            ? "At this moment the service is not available!"
-            : "HTTP error!"
-        } \nStatus: ${pokemon1TypeResponse.status}`
-      );
+      throw new CustomError('First pokemon Type:', pokemon1TypeResponse.status);
     }
-
     const pokemon1TypeJson = await pokemon1TypeResponse.json();
     const pokemon1DamageTo = pokemon1TypeJson.damage_relations.double_damage_to;
     const pokemon2Response = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${pokemon2}`
     );
     if (!pokemon2Response.ok) {
-      throw new Error(
-        `Second pokemon: ${
-          pokemon2Response.status == 404
-            ? "It does not exist, write the name correctly!"
-            : pokemon2Response.status >= 500
-            ? "At this moment the service is not available!"
-            : "HTTP error!"
-        } \nStatus: ${pokemon2Response.status}`
-      );
+      throw new CustomError('Second pokemon:', pokemon2Response.status);
     }
     const pokemon2Json = await pokemon2Response.json();
     const pokemon2PrimaryTypeName = pokemon2Json.types[0].type.name;
     const pokemon2PrimaryTypeUrl = pokemon2Json.types[0].type.url;
     const pokemon2TypeResponse = await fetch(pokemon2PrimaryTypeUrl);
     if (!pokemon2TypeResponse.ok) {
-      throw new Error(
-        `Second pokemon Type: ${
-          pokemon2TypeResponse.status == 404
-            ? "It does not exist."
-            : pokemon2TypeResponse.status >= 500
-            ? "At this moment the service is not available!"
-            : "HTTP error!"
-        } \nStatus: ${pokemon2TypeResponse.status}`
-      );
+      throw new CustomError('Second pokemon Type:', pokemon2TypeResponse.status);
     }
     const pokemon2TypeJson = await pokemon2TypeResponse.json();
     const pokemon2DamageTo = pokemon2TypeJson.damage_relations.double_damage_to;
